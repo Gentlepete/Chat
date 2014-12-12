5772ed3c4c54539cf393fa43b45f120bcc0fce18
Alfa <Alfa@172.16.84.105>
2014-12-12 13:51:42 +0100

    add chat.php add home.php add logout.php

---
+++ chat.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>Dein persönlicher Chat mit [platzhalter]</h1>
+        
+        <div style="border: 1px solid black;width: 300px;height: 500px;margin-left: 100px;">
+            <div style="border: 1px solid black;height: 90%;">
+                Hier wird der Chatverlauf angezigt
+            </div>
+            <div style="border: 1px solid black;height: 10%;">
+                Hier kannst du deine Chatnachricht eintippen und senden
+            </div>
+        </div>
+        <?php
+        
+        // put your code here
+        ?>
+    </body>
+</html>
---
+++ home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+    </head>
+    <body>
+        <a href="logout.php">Lougout</a>
+        <h1>Deine Home Seite</h1>
+        
+        
+        <div style="margin-right: 20px;float:left;border: 1px solid black;width: 150px;height: 500px;">
+          Hier werden Die User aufgelistet
+        </div>
+        <div style="float: left;border: 1px solid black; width: 400px;height:500px;overflow: auto;">
+            <div style="border: 1px solid black;height: 7%;">
+                Hier kannst du einen Beitrag posten
+            </div>    
+            <div style="border: 1px solid black;height: 90%;">
+                Hier werden die geposteten Beiträge angezeigt
+            </div>  
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
