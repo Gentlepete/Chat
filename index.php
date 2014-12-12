1d17491535b29183ecf635b19a0cf72b8bb90f6c
Alfa <Alfa@172.16.84.105>
2014-12-12 13:57:29 +0100

    change index.php & sign_up.html

--- index.php
+++ index.php
@@ -10,6 +10,29 @@ and open the template in the editor.
         <title>Chat</title>
     </head>
     <body>
+        <h1>Ein kleiner, einfach Chat</h1>
+        
+        <form action="login.php" mehod="post">
+            <fieldset>
+                <legend>Gib deinen Usernamen und dein Passwort ein:</legend>
+                <p>
+                    <label>Username </label>
+                    <input name="username">
+                </p>
+                <p>
+                    <label>Passwort </label>
+                    <input type="password" name="password">
+                </p>
+                <p><input type="submit"></p>
+            </fieldset>
+                
+        </form>
+        
+        <fieldset>
+            <legend>Noch keinen Account? Regisrtiere dich hier</legend>
+            <a href="sign_up.html">Registrieren</a>
+            
+        </fieldset>
         <?php
         // put your code here
         ?>
