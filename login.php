41fd0d88208bdc1c2bfdbff7a811b9058ec7b8d3
Alfa <Alfa@172.16.85.57>
2014-12-15 15:52:04 +0100

    get_query_data überarbeitet

--- login.php
+++ login.php
@@ -6,14 +6,9 @@ $con->connect();
 
 $name = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
 $password = md5(filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING));
-echo "<p>Name: $name</p>";
-echo "<p>Password: $password</p>";
 
 $sql = "SELECT * FROM user WHERE name = '$name' AND password = '$password'";
 $res = $con->query($sql);
-
-//$user = $con->get_query_data($res);
-//var_dump($user);
 
 if(!$res)
 {
@@ -23,7 +18,9 @@ if(!$res)
 else
 {
     $user = $con->get_query_data($res);
+    var_dump($user);
     $_SESSION['current_user_id'] = $user['id'];
+    $_SESSION['message'] = "Willkommen in unserem Chat, $name";
     header("Location: home.php");
 }
 
